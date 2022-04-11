8f46015eb6f06bbb7c2ea64145f22a85d4c82821
elonazeqiri <ez48271@ubt-uni.net>
2022-04-12 00:55:01 +0200

    Second Commit

--- src/screens/landingpage/LandingPage.js
+++ src/screens/landingpage/LandingPage.js
@@ -2,13 +2,14 @@ import FirstSection from './FirstSection/FirstSection';
 import FirstSectionStyle from './FirstSection/FirstSectionStyle.css';
 import SecondSection from './SecondSection/SecondSection';
 import './LandingPageStyle.css';
+import ThirdSection from './ThirdSection/ThirdSection';
 
 const LandingPage =()=> {
   return (
 <div  className="landingpage">
    <FirstSection/>
    <SecondSection/>
-   
+   <ThirdSection/>
    </div>
   ); 
 }
