0019feabaadf3419709c5f3a8d6843c241f916b4
elonazeqiri <ez48271@ubt-uni.net>
2022-04-12 00:56:40 +0200

    Third section commit

---
+++ src/screens/landingpage/ThirdSection/ThirdSection.js
@@ -0,0 +1,12 @@
+import './ThirdSectionStyle.css';
+
+const ThirdSection =()=> {
+  return (
+    <div className="ThirdSection">
+     
+     <h1>ThirdSection Page</h1>
+    </div>
+  );
+}
+
+export default ThirdSection;
