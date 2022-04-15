4b10f394dacdb2d386047523e55b5cf9238f8fc5
leonita27 <leonitafetahu27@gmail.com>
2022-04-15 23:50:30 +0200

    feat:header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,48 @@
+import './HeaderStyle.scss';
+import {FaFacebookF} from 'react-icons/fa'
+import {AiOutlineTwitter} from 'react-icons/ai'
+
+const Header =()=> {
+  return (
+    <div className="headerContainer"> 
+    <div className="middleContainer">
+    <div className="logo">
+      <h1 className="logoName">FITNESS</h1>
+      <h1 className="logoDetail">XTREME</h1>
+    </div>
+    <div className="navBar">
+        <ul>
+          <li>
+            <a href="/about">ABOUT US</a>
+          </li>
+          <li>
+            <a href="/classes">OUR CLASSES</a>
+          </li>
+          <li>
+            <a href="/gallery">GALLERY</a>
+          </li>
+          <li>
+            <a href="/trainers">TRAINERS</a>
+          </li>
+          <li>
+            <a href="/team">TEAM</a>
+          </li>
+       </ul>
+    </div>
+    <div className="lastContainer">
+    <div className='iconBox'>
+    <FaFacebookF  color='white' size={20} />
+    </div>
+    <div className='iconBox'>
+    <AiOutlineTwitter color='white' size={20} />
+    </div>
+    <div className='iconBox'>
+    <h1 className='languageText'>EN</h1>
+    </div>
+    </div>
+    </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/screens/landingpage/LandingPage.js
+++ src/screens/landingpage/LandingPage.js
@@ -2,10 +2,12 @@ import FirstSection from './FirstSection/FirstSection';
 import SecondSection from './SecondSection/SecondSection';
 import './LandingPageStyle.scss';
 import ThirdSection from './ThirdSection/ThirdSection';
+import Header from '../../components/Header';
 
 const LandingPage =()=> {
   return (
 <div  className="landingpage">
+   <Header/>
    <FirstSection/>
    <SecondSection/>
    <ThirdSection/>
