4f59e4b6200bf19e46ca9290b5be419b73752b29
leonita27 <leonitafetahu27@gmail.com>
2022-04-18 00:22:39 +0200

    fix:eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,26 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  rules: {
-    "react/react-in-jsx-scope": "off",
-  },
-  plugins: ["prettier"],
+   env: {
+      browser: true,
+      es2021: true,
+      jest: true,
+   },
+   extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:prettier/recommended',
+   ],
+   parserOptions: {
+      ecmaFeatures: {
+         jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+   },
+   plugins: ['react'],
+   rules: {
+      'react/react-in-jsx-scope': 'off',
+      'prettier/prettier': 'error',
+   },
+   plugins: ['prettier'],
+   extends: 'react-app',
 }
