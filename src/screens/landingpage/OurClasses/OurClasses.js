785c03c95f5bea2094c70d66579c71aae76f5ec0
elonazeqiri <ez48271@ubt-uni.net>
2022-04-21 22:28:45 +0200

    Added multi-language for our classes section

--- src/screens/landingpage/OurClasses/OurClasses.js
+++ src/screens/landingpage/OurClasses/OurClasses.js
@@ -4,45 +4,48 @@ import OurClasses1 from "../../../assets/images/landpage/OurClasses1.png"
 import OurClasses2 from "../../../assets/images/landpage/OurClasses2.png"
 import OurClasses3 from "../../../assets/images/landpage/OurClasses3.png"
 import OurClasses4 from "../../../assets/images/landpage/OurClasses4.jpg"
+import { useTranslation } from "react-i18next"
 
 const OurClasses = () => {
+  const { t } = useTranslation()
   return (
     <div className="OurClassContainer">
       <div className="fistContainer">
         <img src={OurClasses1} width={"70%"} alt="" />
       </div>
       <div className="secondContainer">
-        <h1 className="OurClassesTitle">Our Classes</h1>
+        <h1 className="OurClassesTitle">{t("LandingPage.OurClasses.title")}</h1>
         <p className="OurClassesDescription">
-        Fitness Classes are a great way to get started, make progress on your 
-            goals or change up your routine. Our certified instructors make sure 
-                you get motivated, learn proper form and have fun.
+          {t("LandingPage.OurClasses.subTitle")}
         </p>
         <div className="photosContainer">
           <div className="fistText">
             <img src={OurClasses2} width={"20%"} alt="" />
             <h1 className="headerText">
-              Weight Gain
+              {t("LandingPage.OurClasses.firstSubtitle")}
             </h1>
             <p className="paragraphText">
-              Weight gain can be caused by different things, among them muscle gain. 
-              Certain exercises that focus on building muscle will help you gain weight. 
+              {t("LandingPage.OurClasses.firstDescription")}
             </p>
           </div>
-        <div className="fistText">
-          <img src={OurClasses3} width={"20%"} alt="" />  
-          <h1 className="headerText">Weight Loss</h1>
-          <p className="paragraphText">Losing weight is not easy, and it takes commitment. 
-                But if you’re ready to get started, we’ve got a step-by-step guide to help get 
-                you on the road to weight loss and better health.</p>
-        </div>
-        <div className="fistText">
-          <img src={OurClasses4} width={"20%"} alt="" />
-          <h1 className="headerText">Muscle Dfn</h1>
-          <p className="paragraphText">If your goal is more-defined muscles, what will get you
-               there is being smart about your goals, doing the right types of workouts, 
-               eating right, and getting the rest your body needs.</p>
-        </div>
+          <div className="fistText">
+            <img src={OurClasses3} width={"20%"} alt="" />
+            <h1 className="headerText">
+              {t("LandingPage.OurClasses.secondSubtitle")}
+            </h1>
+            <p className="paragraphText">
+              {t("LandingPage.OurClasses.secondDescription")}
+            </p>
+          </div>
+          <div className="fistText">
+            <img src={OurClasses4} width={"20%"} alt="" />
+            <h1 className="headerText">
+              {t("LandingPage.OurClasses.thirdSubtitle")}
+            </h1>
+            <p className="paragraphText">
+              {t("LandingPage.OurClasses.thirdDescription")}
+            </p>
+          </div>
         </div>
       </div>
     </div>
